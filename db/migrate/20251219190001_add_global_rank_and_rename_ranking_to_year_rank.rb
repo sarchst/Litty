e0dadde4f4dafb34759f1dd0a8a7827b0e03d729
Sarchen Starke <sarchen.starke@later.com>
2025-12-19 13:14:57 -0700

    Fix migration: rename ranking to year_rank correctly

--- db/migrate/20251219190001_add_global_rank_and_rename_ranking_to_year_rank.rb
+++ db/migrate/20251219190001_add_global_rank_and_rename_ranking_to_year_rank.rb
@@ -1,7 +1,7 @@
 class AddGlobalRankAndRenameRankingToYearRank < ActiveRecord::Migration[8.0]
   def change
-    # Rename existing year_rank column to year_rank
-    rename_column :books, :year_rank, :year_rank
+    # Rename existing ranking column to year_rank
+    rename_column :books, :ranking, :year_rank
     
     # Add new global_rank column
     add_column :books, :global_rank, :integer
